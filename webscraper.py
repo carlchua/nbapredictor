c94f36f611859f6573670c5ba8b5fc77efdbae12
Carl Chua <carlchua@berkeley.edu>
2020-09-17 06:57:40 +0800

    mostly done with webscraper

--- webscraper.py
+++ webscraper.py
@@ -2,8 +2,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
-import tkinter as tk
-from tkinter import filedialog
+import re
 
 # REGULAR SEASON
 month = ['october', 'november', 'december', 'january', 'february', 'march']
@@ -34,24 +33,34 @@ for year in range(2017,2020)
 
             t1_b_header = [th.getText() for th in team1_basic.findAll('tr', limit=3)[1].findAll('th')][1:]
             r1b = team1_basic.findAll('tr')[2:]
-            stats_1b = [[td.getText() for td in r1b[i].findAll('td')] for i in range(len(r1b))]
-            df1b = pd.DataFrame(stats_1b, columns = r1_b_header)
+            stats_1b = [[td.getText() for td in r1b[i].findAll('td')] for i in range(len(r1b)) if i!=5]
+            df1b = pd.DataFrame(stats_1b, columns = t1_b_header)
 
             t1_a_header = [th.getText() for th in team1_adv.findAll('tr', limit=3)[1].findAll('th')][1:]
-            r1a = team1_basic.findAll('tr')[2:]
-            stats_1a = [[td.getText() for td in r1a[i].findAll('td')] for i in range(len(r1a))]
-            df1a = pd.DataFrame(stats_1a, columns = r1_a_header)
+            r1a = team1_adv.findAll('tr')[2:]
+            stats_1a = [[td.getText() for td in r1a[i].findAll('td')] for i in range(len(r1a)) if i!=5]
+            df1a = pd.DataFrame(stats_1a, columns = t1_a_header)
 
             t2_b_header = [th.getText() for th in team2_basic.findAll('tr', limit=3)[1].findAll('th')][1:]
-            r2b = team1_basic.findAll('tr')[2:]
-            stats_2b = [[td.getText() for td in r2b[i].findAll('td')] for i in range(len(r2b))]
-            df2b = pd.DataFrame(stats_2b, columns = r2_b_header)
+            r2b = team2_basic.findAll('tr')[2:]
+            stats_2b = [[td.getText() for td in r2b[i].findAll('td')] for i in range(len(r2b)) if i!=5]
+            df2b = pd.DataFrame(stats_2b, columns = t2_b_header)
 
 
             t2_a_header = [th.getText() for th in team2_adv.findAll('tr', limit=3)[1].findAll('th')][1:]
-            r2a = team1_basic.findAll('tr')[2:]
-            stats_2a = [[td.getText() for td in r2a[i].findAll('td')] for i in range(len(r2a))]
-            df2a = pd.DataFrame(stats_2a, columns = r2_a_header)
+            r2a = team2_adv.findAll('tr')[2:]
+            stats_2a = [[td.getText() for td in r2a[i].findAll('td')] for i in range(len(r2a)) if i!=5]
+            df2a = pd.DataFrame(stats_2a, columns = t2_a_header)
+
+            mydivs = soup2.findAll("div", {"class": "scores"})
+            score_arr = []
+            for d in mydivs:
+                score_arr.append(d.get_text())
+
+            score_arr[0] = re.sub("[^0-9]", "", score_arr[0])
+            score_arr[1] = re.sub("[^0-9]", "", score_arr[1])
+
+
 
 
 
